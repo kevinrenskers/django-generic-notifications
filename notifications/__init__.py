9cba92c30aaed8563afac830509d32cb4f996b84
Kevin Renskers <info@bolhoed.net>
2011-11-09 16:51:06 +0100

    Added version to __init__.py

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,3 +1,12 @@
+VERSION = (0, 1)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    return version
+
+
 class AlreadyRegistered(Exception):
     pass
 
