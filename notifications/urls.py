3f58e72fedd333484249aac46010a3d752587633
Kevin Renskers <info@bolhoed.net>
2011-12-06 15:27:39 +0100

    0.2: December 6, 2011 - Define __getattr__ for easy access to kwargs - Start of new settings app, where users can select which types they're interested in - Users can also select backends per type

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
 from notifications.views import IndexView
 
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='notification-settings-index'),
+    url(r'^$', login_required(IndexView.as_view()), name='notification-settings-index'),
 )
